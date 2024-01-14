4893fd6cae1bc7779f7cb20b28a0e4900ec61069
sabzj <zjsabbah@gmail.com>
2024-01-14 16:36:45 +0200

    updated backend

---
+++ backEnd/controller/activityControler.js
@@ -0,0 +1,97 @@
+import Activity from "../models/activitySchema.js";
+import STATUS_CODE from "../constants/statusCodes.js";
+
+export const createActivity = async (req, res, next) => {
+  try {
+    const { name, description, category, location } = req.body;
+    const newActivity = await Activity.create({
+      name,
+      description,
+      category,
+      location,
+    });
+    res.status(STATUS_CODE.CREATED);
+    res.send(newActivity);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getActivityById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const activity = await Activity.findById(id).populate("announcer");
+    if (!activity) {
+      res.status(STATUS_CODE.NOT_FOUND);
+      throw new Error("No such activity in the db");
+    }
+    res.send(activity);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllActivities = async (req, res, next) => {
+  try {
+    const activities = await Activity.find({}).populate("annoncer"); // Populate the 'announcer' field
+    res.status(STATUS_CODE.OK);
+    res.send(activities);
+  } catch (error) {
+    next(error);
+  }
+};
+// check if the volunteer activity is announced by more than one
+export const announcedActivity = async (req, res, next) => {
+  try {
+    const { userId } = req.body;
+    const { activityId } = req.params;
+    const activity = await Activity.findById(activityId);
+    if (activity.announcer) {
+      res.status(STATUS_CODE.CONFLICT);
+      throw new Error("Activity already has been announced");
+    }
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { $push: { activities: activityId } },
+      { new: true }
+    ).populate("activities");
+
+    if (!user) {
+      res.status(STATUS_CODE.NOT_FOUND);
+      throw new Error("No such user in the db");
+    }
+    activity.announcer = userId;
+    await activity.save();
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Controller to delete an activity by id
+export const deleteActivity = async (req, res, next) => {
+  try {
+    const { activityId } = req.params;
+
+    // Find the activity to get the onnouncer's information
+    const activity = await Activity.findById(carId);
+    if (!activity) {
+      res.status(STATUS_CODE.NOT_FOUND);
+      throw new Error("No such activity in the db");
+    }
+
+    // If the car has an owner, remove the car from the owner's list
+    if (activity.owner) {
+      await User.findByIdAndUpdate(activity.owner, {
+        $pull: { activities: activity._id },
+      });
+    }
+
+    // Delete the car from the store
+    await Activity.deleteOne({ _id: activity._id });
+
+    res.send(`Car with id ${activityId} was deleted successfully`);
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ backEnd/routes/activityRoutes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router = Router();
+
+// router.get("/", getAllActivities);
+// router.get("/:id", getActivityById);
+// router.post("/", createActivity);
+// router.put("/:id", updateActivity);
+// router.delete("/:id", deleteActivity);
+
+export default router;
